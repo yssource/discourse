21bdb7c511915f5c7560c91ee49c99ecbb5199c8
Loïc Guitaut <loic@discourse.org>
2021-10-06 15:26:32 +0200

    WIP: oneboxes working and autorefreshing

--- app/assets/javascripts/discourse/app/models/pending-post.js
+++ app/assets/javascripts/discourse/app/models/pending-post.js
@@ -4,27 +4,16 @@ import RestModel from "discourse/models/rest";
 import { userPath } from "discourse/lib/url";
 import { alias } from "@ember/object/computed";
 import { cookAsync } from "discourse/lib/text";
-import { loadOneboxes } from "discourse/lib/load-oneboxes";
-import { ajax } from "discourse/lib/ajax";
-import { resolveAllShortUrls } from "pretty-text/upload-short-url";
 
 const PendingPost = RestModel.extend({
   expandedExcerpt: null,
   postUrl: alias("topic_url"),
+  truncated: false,
 
   init() {
     this._super(...arguments);
     cookAsync(this.raw_text).then((cooked) => {
       this.set("expandedExcerpt", cooked);
-      loadOneboxes(
-        this.element,
-        ajax,
-        this.topic_id,
-        this.category_id,
-        this.siteSettings.max_oneboxes_per_post,
-        false
-      );
-      resolveAllShortUrls(ajax, this.siteSettings, this.element, this.opts);
     });
   },
 
